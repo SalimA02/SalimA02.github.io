a76d428e4629aed49d988d74de482af293c385c6
SalimA02 <52701638+SalimA02@users.noreply.github.com>
2022-09-15 13:18:42 +0100

    Translate

---
+++ Translate/script.js
@@ -0,0 +1,77 @@
+const fromText = document.querySelector(".from-text"),
+toText = document.querySelector(".to-text"),
+selectTag = document.querySelectorAll("select"),
+
+translateBtn = document.querySelector("button");
+
+exchageIcon = document.querySelector(".exchange"),
+
+icons  = document.querySelectorAll(".row i");
+
+selectTag.forEach((tag,id) => {
+    for (const country_code in countries) {
+        //selecting English by default, to Spanish
+        let selected;
+        if(id == 0 && country_code == "en"){
+            selected = "selected";
+        } else if (id == 1 && country_code == "es"){
+            selected = "selected"
+        }
+
+        let option = `<option value="${country_code}" ${selected}>${countries[country_code]}</option>`;
+        tag.insertAdjacentHTML("beforeend", option) //adding options tag inside select tag
+        
+    }
+});
+
+
+translateBtn.addEventListener("click", () =>{
+    let text =  fromText.value;
+    translateFrom = selectTag[0].value,
+    translateTo = selectTag[1].value;
+
+    if(!text) return;
+    toText.setAttribute("placeholder", "Translating...")
+    
+    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo} `;
+    fetch(apiUrl).then(res => res.json()).then(data => {
+        console.log(data);
+        toText.value = data.responseData.translatedText;
+        toText.setAttribute("placeholder", "Translating...")
+    })
+})
+
+exchageIcon.addEventListener("click", () =>{
+    let tempText = fromText.value,
+    tempLang = selectTag[0].value;
+
+    fromText.value = toText.value,
+    selectTag[0].value = selectTag[1].value;
+
+    toText.value = tempText;
+    selectTag[1].value = tempLang
+})
+
+
+icons.forEach(icon => {
+    icon.addEventListener("click", ({target}) => {
+        if(target.classList.contains("fa-copy")) {
+            if(target.id == "from"){
+                navigator.clipboard.writeText(fromText.value)
+            } else {
+                navigator.clipboard.writeText(toText.value)
+
+            }
+        }else {
+            let utterance;
+            if(target.id == "from"){
+                utterance = new SpeechSynthesisUtterance(fromText.value)
+                utterance.lang = selectTag[0].value;
+            } else {
+                utterance = new SpeechSynthesisUtterance(toText.value)
+                utterance.lang = selectTag[1].value;
+            }
+            speechSynthesis.speak(utterance)
+        }
+    })
+})
