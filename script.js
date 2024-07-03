9ffb9cff5a03900ac6e8ad37809cd5ae23544181
SalimA02 <52701638+SalimA02@users.noreply.github.com>
2024-07-03 11:52:05 +0100

    Update Scroll

--- script.js
+++ script.js
@@ -1,71 +1,83 @@
-$(document).ready(function()
-{
-    $(window).scroll(function()
-    {
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky")
-        } 
-        else{
-            $('.navbar').removeClass("sticky")
+$(document).ready(function() {
+    // Window scroll event
+    $(window).scroll(function() {
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
         }
 
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show")
-        }
-        else{
-            $('.scroll-up-btn').removeClass("show")
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
         }
     });
 
-    // SLIDE UP SCRIPT
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0})
+    // Slide up script
+    $('.scroll-up-btn').click(function() {
+        $('html').animate({ scrollTop: 0 });
     });
-        
-    // TOGGLE MENU / NAVBAR SCRIPT
 
-    $('.menu-btn').click(function(){
+    // Toggle menu / navbar script
+    $('.menu-btn').click(function() {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
-
     });
 
-    // TYPING ANIMATION SCRIPT
-
-    var typed = new Typed(".typing",{
-        strings: ['Computer Scientist',"Web Developer", "Software Engineer"],
+    // Typing animation script
+    var typedOptions = {
+        strings: ['Computer Scientist', "Web Developer", "Software Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
-    });
+    };
 
-    var typed = new Typed(".typing-2",{
-        strings: ["Computer Scientist", "Web Developer", "Software Engineer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+    new Typed(".typing", typedOptions);
+    new Typed(".typing-2", typedOptions);
 
-    // OWL CAROUSEL SCRIPT
-
+    // Owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1,
                 nav: false
             },
-            600:{
+            600: {
                 items: 2,
                 nav: false
             },
-            1000:{
+            1000: {
                 items: 3,
-                nav: false 
+                nav: false
             }
         }
-    })
+    });
+
+    // Scroll position for sections
+    $('.menu a').click(function(event) {
+        event.preventDefault();
+        var target = $(this).attr('href');
+        var offset = 0; // Default offset
+
+        if (target === '#home') {
+            offset = 0;
+        } else if (target === '#about') {
+            offset = 100;
+        } else if (target === '#services') {
+            offset = 50;
+        } else if (target === '#projects') {
+            offset = 110;
+        } else {
+            offset = 90;
+        }
+
+        $('html, body').animate({
+            scrollTop: $(target).offset().top + offset
+        }, 500);
+    });
 });
