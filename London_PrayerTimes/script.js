f223be39043506440dd8d0dc044f29ab0d919044
SalimA02 <52701638+SalimA02@users.noreply.github.com>
2025-01-05 20:45:36 +0000

    Update script.js

--- London_PrayerTimes/script.js
+++ London_PrayerTimes/script.js
@@ -7,7 +7,7 @@ async function getData(latitude, longitude){
 
     
 
-    const apiUrl = `https://api.aladhan.com/v1/calendar/${year}/${month}?latitude=${latitude}&longitude=${longitude}&method=2&tune=0,5,0,6,2,3,-26`;
+    const apiUrl = `https://api.aladhan.com/v1/calendar/${year}/${month}?latitude=${latitude}&longitude=${longitude}&method=2&tune=0,4,0,6,2,2,-28`;
     // API call with tuning to match Islamic Relief prayer times
 
     const response = await fetch(apiUrl);
