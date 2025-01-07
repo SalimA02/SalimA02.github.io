90ba4e8ed5a6075b032b15209adcf3ac402b2fa3
SalimA02 <52701638+SalimA02@users.noreply.github.com>
2025-01-07 19:42:43 +0000

    Update script.js
    HTTPS

--- London_PrayerTimes/script.js
+++ London_PrayerTimes/script.js
@@ -4,7 +4,7 @@ async function fetchPrayerTimes() {
   try {
       const apiKey = '#########'; // Replace with your actual API key
       const format = 'json';
-      const url = `http://www.londonprayertimes.com/api/times/?format=${format}&key=${apiKey}`;
+      const url = `https://www.londonprayertimes.com/api/times/?format=${format}&key=${apiKey}`;
 
       const response = await fetch(url);
 
