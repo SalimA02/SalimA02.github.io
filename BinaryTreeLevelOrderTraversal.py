ca161ee81b4392c593b0e510c4de14a32b0721b7
SalimA02 <52701638+SalimA02@users.noreply.github.com>
2025-04-06 21:36:46 +0100

    Create BinaryTreeLevelOrderTraversal.py

---
+++ BinaryTreeLevelOrderTraversal.py
@@ -0,0 +1,16 @@
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+
+        def dfs(node, depth):
+            if not node:
+                return None
+            if len(res) == depth:
+                res.append([])
+
+            res[depth].append(node.val)
+            dfs(node.left, depth + 1)
+            dfs(node.right, depth + 1)
+
+        dfs(root, 0)
+        return res
