4fd030155b047fb5393f58422a674a509714ea17
SalimA02 <52701638+SalimA02@users.noreply.github.com>
2026-07-19 15:01:31 +0100

    Move weather API calls to Cloudflare Worker proxy

--- Weather/script.js
+++ Weather/script.js
@@ -1,15 +1,13 @@
 
 let weather = {
-    apiKey: "#########",
     fetchWeather: function (city){
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
-        + city 
-        + "&units=metric&appid=" 
-        + this.apiKey
-        )   
-            .then((response) => response.json())
-            .then((data) => this.displayWeather(data));
-    },
+        fetch(
+            "https://salim-api-proxy.salimahmed02.workers.dev/weather?city="
+            + city
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+        },
 
     displayWeather: function(data){
 
